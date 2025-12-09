07dc034248f0cd0b6657bb4797b402e3a306fcac
Sam-ys42 <ysdelacruzm01@educarex.es>
2025-12-09 18:00:19 +0100

    Backup automático (2025-12-09 18:00:18)

---
+++ Repaso_Acceso_Datos/src/NavalVinted/Ej3_Leer_RandomAccess.java
@@ -0,0 +1,60 @@
+package NavalVinted;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+public class Ej3_Leer_RandomAccess {
+
+	public static Scanner teclado = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Introduce el id del producto: ");
+		
+		int id = Integer.parseInt(teclado.nextLine());
+
+		try {
+			RandomAccessFile archivo = new RandomAccessFile(
+					"precio.dat", "rw");
+
+			try {
+
+				archivo.seek(0);
+				double beneficio;
+
+				while (true) {
+
+					if (id == archivo.readInt()) {
+
+						double precio = archivo.readDouble();
+						double coste = archivo.readDouble();
+						int descuento = archivo.readInt();
+						
+						 beneficio = (precio - (precio * descuento / 100) - coste);
+						break;
+
+					} else {
+
+						archivo.skipBytes(20);
+
+					}
+				}
+
+				System.out.println("El benificio del producto con id: " + id + " es  " + beneficio);
+
+			} catch (EOFException e) {
+				// TODO: handle exception
+				System.out.println("id desconocido.");
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ Repaso_Acceso_Datos/src/NavalVinted/Ej5_Escribir_JSON.java
@@ -0,0 +1,33 @@
+package NavalVinted;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class Ej5_Escribir_JSON {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			ObjectInputStream ois1 = new ObjectInputStream(
+					new FileInputStream("ropa.dat"));
+			Productos lista = (Productos) ois1.readObject();
+			ois1.close();
+
+			// Mostrar lista (comprobación)
+			lista.mostrarProductos();
+
+			ObjectMapper mapeador = new ObjectMapper();
+			mapeador.enable(SerializationFeature.INDENT_OUTPUT);
+
+			mapeador.writeValue(new File("ropa.json"), lista);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+
+}
