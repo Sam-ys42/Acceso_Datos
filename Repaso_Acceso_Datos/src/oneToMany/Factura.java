57dc83ff399775e7d12d1ff269d7b5d4dd227fb6
Sam-ys42 <ysdelacruzm01@educarex.es>
2026-02-12 15:42:09 +0100

    Backup automático (2026-02-12 15:42:07)

---
+++ Repaso_Acceso_Datos/src/oneToMany/Factura.java
@@ -0,0 +1,73 @@
+package oneToMany;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Factura")
+public class Factura {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private Date fecha;
+	private String concepto;
+	private Double importe;
+	@ManyToOne
+	@JoinColumn(name = "persona_nombre")
+	private Persona persona;
+	
+	
+	public Factura(int id, Date fecha, String concepto, Double importe, Persona persona) {
+		super();
+		this.id = id;
+		this.fecha = fecha;
+		this.concepto = concepto;
+		this.importe = importe;
+		this.persona = persona;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getConcepto() {
+		return concepto;
+	}
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
+	public Double getImporte() {
+		return importe;
+	}
+	public void setImporte(Double importe) {
+		this.importe = importe;
+	}
+	
+	public Factura() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Factura [id=" + id + ", fecha=" + fecha + ", concepto=" + concepto + ", importe=" + importe
+				+ ", persona=" + persona + "]";
+	}
+	
+	
+	
+}
---
+++ Repaso_Acceso_Datos/src/oneToMany/Persona.java
@@ -0,0 +1,59 @@
+package oneToMany;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Persona")
+public class Persona {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nombre;
+	@OneToMany(mappedBy = "persona")
+	private List<Factura> facturas;
+	public Persona(int id, String nombre, List<Factura> facturas) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.facturas = facturas;
+	}
+	public Persona() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", nombre=" + nombre + ", facturas=" + facturas + "]";
+	}
+	
+	
+	
+	
+	
+
+}
